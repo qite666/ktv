3039ddb047fdf3d1d08ad56f1a7851f135a6c336
qite666 <15243986018@139.com>
2021-06-11 10:50:52 +0800

    初步创建

--- ktv/src/router/index.js
+++ ktv/src/router/index.js
@@ -1,22 +1,63 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/main'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/main',
+    name: 'Main',
+    component: () => import('../views/Main.vue'),
+    children: [
+      {
+        path: '/',
+        redirect: '/index'
+      },
+      {
+        path: '/index',
+        name: 'Index',
+        component: () => import('../components/Index.vue')
+      }
+      // {
+      //   path: '/aboutus',
+      //   name: 'AboutUs',
+      //   component: () => import('../components/AboutUs.vue')
+      // },
+      // {
+      //   path: '/environmental',
+      //   name: 'Environmental',
+      //   component: () => import('../components/Environmental.vue')
+      // },
+      // {
+      //   path: '/journalism',
+      //   name: 'Journalism',
+      //   component: () => import('../components/Journalism.vue')
+      // },
+      // {
+      //   path: '/modelshow',
+      //   name: 'ModelShow',
+      //   component: () => import('../components/ModelShow.vue')
+      // },
+      // {
+      //   path: '/newsdetails/:id',
+      //   name: 'NewsDetails',
+      //   component: () => import('../components/NewsDetails.vue')
+      // },
+      // {
+      //   path: '/environmentaldetails/:id',
+      //   name: 'EnvironmentalDetails',
+      //   component: () => import('../components/EnvironmentalDetails.vue')
+      // },
+      // {
+      //   path: '/modelshow2/:id',
+      //   name: 'ModelShow2',
+      //   component: () => import('../components/ModelShow2.vue')
+      // }
+    ]
   }
 ]
 
---
+++ ktv/src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <my-head></my-head>
+        <keep-alive exclude="newsdetails,aboutus,environmentaldetails">
+            <router-view></router-view>
+        </keep-alive>
+        <my-foot></my-foot>
+    </div>
+</template>
+<script>
+import myHead from './Header'
+import myFoot from './Footer'
+export default {
+    components: {
+        myHead: myHead,
+        myFoot: myFoot
+    }
+}
+</script>
